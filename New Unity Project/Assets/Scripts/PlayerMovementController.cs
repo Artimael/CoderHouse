2ebf1e76c3b5db52beb11fbd12da0c6b1f23cb12
Wei <weichongl@hotmail.com>
2021-11-02 20:47:00 -0300

    Desafio 9
    Se agrega logica para desafio 9
    
    tanto colision
    Como portal

--- New Unity Project/Assets/Scripts/PlayerMovementController.cs
+++ New Unity Project/Assets/Scripts/PlayerMovementController.cs
@@ -6,35 +6,27 @@ public class PlayerMovementController : MonoBehaviour
 {
     public float speed;
     public float cameraAxisX;
+    private float tiempoColision=0.0f;
+    private GameObject wall;
+    [SerializeField] bool esCruzarPortal=false;
+    private int contador=0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        wall = GameObject.Find("Wall");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W)){
-            movePlayer(Vector3.forward);
-        }
-
-        if(Input.GetKey(KeyCode.S)){
-            movePlayer(Vector3.back);
-        }
-
-        if(Input.GetKey(KeyCode.A)){
-            movePlayer(Vector3.left);
-        }
-
-        if(Input.GetKey(KeyCode.D)){
-            movePlayer(Vector3.right);
-        }
+        movePlayer();
         RotatePlayer();
     }
 
-    void movePlayer(Vector3 direction){
-        transform.Translate(speed* Time.deltaTime * direction);
+    void movePlayer(){
+        float ejeHorizontal = Input.GetAxisRaw("Horizontal");
+        float ejeVertical = Input.GetAxisRaw("Vertical");
+        transform.Translate(speed * Time.deltaTime * new Vector3(ejeHorizontal, 0, ejeVertical));
     }
 
     private void RotatePlayer()
@@ -43,6 +35,66 @@ public class PlayerMovementController : MonoBehaviour
         Quaternion angulo   = Quaternion.Euler(0, cameraAxisX, 0);
         transform.localRotation = angulo;
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);    
+    }
+
+    private void OnCollisionStay(Collision other){
+
+        if (other.gameObject.CompareTag("Muro"))
+        {
+           tiempoColision=tiempoColision+Time.deltaTime;
+        } 
+
+        if(tiempoColision>2.0f){
+            Debug.Log("Colisionaste mas de 2f");
+            Debug.Log(wall.transform.position);
+            Vector3 nuevaPosicion= new Vector3(Random.Range(-10f, 10f),transform.position.y,Random.Range(-10f, 10f));
+            wall.transform.position=nuevaPosicion;
+            tiempoColision=0.0f;
+        }
+ 
+
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Vector3 mitadScale= new Vector3(0.5f,0.5f,0.5f);
+        Vector3 scaleOriginal= new Vector3(1f,1f,1f);
+        Debug.Log("other.tag "+other.tag);
+        Debug.Log("esCruzarPortal "+esCruzarPortal);
+
+        if(contador<2){
+            if (other.tag == "Portal" && !esCruzarPortal)
+            {
+                Debug.Log("Entre  ");
+                transform.localScale = mitadScale;
+                esCruzarPortal = true;
+                contador++;
+            }
+
+            else
+            //if( other.tag == "Portal" && esCruzarPortal)
+            {
+                Debug.Log("Entre  asdf");
+                transform.localScale = scaleOriginal;
+                esCruzarPortal = false;
+                contador++;
+            }
+        }else{
+            contador=0;
+        }
+
+
+        Debug.Log("esCruzarPortal Saliendo "+esCruzarPortal);
+
+    }
+
+
+
 
 
 }
